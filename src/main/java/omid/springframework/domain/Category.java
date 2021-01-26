e068ff2740e1f635b68a53387cf5a1222ae20699
omid-joukar <joukaromid6@gmail.com>
2021-01-26 12:29:46 +0100

    fixing Typo.

--- src/main/java/omid/springframework/domain/Category.java
+++ src/main/java/omid/springframework/domain/Category.java
@@ -2,11 +2,13 @@ package omid.springframework.domain;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@Entity
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/omid/springframework/repositories/CategoryRepository.java
+++ src/main/java/omid/springframework/repositories/CategoryRepository.java
@@ -2,7 +2,7 @@ package omid.springframework.repositories;
 
 import omid.springframework.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
