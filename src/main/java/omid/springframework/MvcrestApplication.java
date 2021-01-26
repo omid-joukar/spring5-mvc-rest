ecd2a92ad9f9770395fe1d434a3293de9da01032
omid-joukar <joukaromid6@gmail.com>
2021-01-26 11:50:52 +0100

    assignment new project.

---
+++ src/main/java/omid/springframework/MvcrestApplication.java
@@ -0,0 +1,13 @@
+package omid.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcrestApplication.class, args);
+    }
+
+}
